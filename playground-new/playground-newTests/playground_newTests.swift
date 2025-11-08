027221b5d2d1d5d5536a1817fec20af34a2d33d6
Ananta Pandey <pandananta@users.noreply.github.com>
2025-11-07 22:32:03 -0800

    add screen recording and xcodeproj

---
+++ playground-new/playground-newTests/playground_newTests.swift
@@ -0,0 +1,16 @@
+//
+//  playground_newTests.swift
+//  playground-newTests
+//
+//  Created by ananta on 11/7/25.
+//
+
+import Testing
+
+struct playground_newTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
